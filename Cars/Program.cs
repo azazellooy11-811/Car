8c1d4d3d4b53090b436209e20e6be64e2314588b
azazellooy <khismatova_05@mail.ru>
2022-07-28 23:44:18 +0300

    все машины

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PassengerCar passengerCar = new PassengerCar("легковой автомобиль", 8,42,120,300,1);
+            passengerCar.GetDistance();
+            passengerCar.GetPowerReserve();
+            Truck truck = new Truck("грузовой автомобиль", 8, 42, 120,50, 230);
+            truck.GetPowerReserve(230);
+            passengerCar.GetTime();
+        }
+    }
+}
